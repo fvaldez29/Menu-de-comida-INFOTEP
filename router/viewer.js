05af37a3afbf67f64da46cf44b85e809b5c003e7
fvaldez29 <fv.2900@hotmail.com>
2024-12-03 23:30:43 -0400

    some changed

--- router/viewer.js
+++ router/viewer.js
@@ -6,5 +6,9 @@ viewRouter.get('/login', (req, res) => {
     res.render('index', { title: "Login", user: req.user })
 })
 
+viewRouter.get('/register', (req, res) => {
+    res.render('pages/register', {title: 'Register Form'})
+})
+
 
 export default viewRouter
